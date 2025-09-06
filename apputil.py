ac64a36d79291ea7f264d0f84732a733a0d9019c
Megha Vadhva <megha.vadhva89@gmail.com>
2025-09-06 01:46:18 +0000

    Test 1

--- apputil.py
+++ apputil.py
@@ -1,13 +1,11 @@
-import numpy as np
-
-
-# update/add code below ...
-
 def ways(n):
-    return None
-
-def lowest_score(names, scores):
-    return None
-
-def sort_names(names, scores):
-    return None
+    """
+    Returns the number of ways to make n cents using pennies (1c) and nickels (5c).
+    """
+    count = 0
+    for nickels in range(n // 5 + 1):
+        pennies = n - 5 * nickels
+        if pennies >= 0:
+            count += 1
+    return count
+print(ways(10))  # Example usage
